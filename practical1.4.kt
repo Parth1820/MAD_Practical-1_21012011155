d07ca2fc23cacfb4d4d468f1bcbf1167988193c7
JENNY <nandaniyajeni@gmail.com>
2023-07-20 23:07:28 +0530

    1.4

---
+++ practical1.4.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    println("21012011155-PARTH")
+    println("Enter a Number: ");
+    val n1 = Integer.valueOf(readLine())
+
+    if(n1 % 2 == 0)
+        println("$n1 is an Even Number")
+    else
+        println("$n1 is an Odd Number")
+}
