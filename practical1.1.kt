c8ecdfb4c64b8cf1abfd1bdc2649fe94a4d10183
JENNY <nandaniyajeni@gmail.com>
2023-07-20 22:50:15 +0530

    1.1

---
+++ practical1.1.kt
@@ -0,0 +1,26 @@
+fun main()
+{
+    println("21012011155-PARTH")
+    var In: Int = 10
+    val I: Int = 5
+    val D: Double = 5.99
+    val C: Char = 'N'
+    val B: Boolean = true
+    val S: String = "Hello"
+    val F: Float = 5.75F
+    val L: Long = 15000000000L
+    val Sh: Short = 5000
+    val By: Byte = 100
+
+    println("$In is variable integer")
+    println("$I is integer")
+    println("$D is double")
+    println("$C is character")
+    println("$B is boolean")
+    println("$S is string")
+    println("$F is float")
+    println("$L is long")
+    println("$Sh is short")
+    println("$By is byte")
+
+}
