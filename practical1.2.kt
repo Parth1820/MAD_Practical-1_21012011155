6753a0cac15533e4ddad997b1821e9c302db1ba3
JENNY <nandaniyajeni@gmail.com>
2023-07-20 22:58:44 +0530

    1.2

---
+++ practical1.2.kt
@@ -0,0 +1,24 @@
+fun main()
+{
+    println("21012011155-PARTH")
+    println("Hello")
+    var a:Int=6
+    println("$a :Integer value")
+    var b:Float=4.5F
+    println("$b :Float value")
+    var c:Char='P'
+    println("$c :Character value")
+    var d:String="PJV"
+    println("$c :String value")
+    var e:Double=96.36
+    println("$e :Double value")
+    var f:Boolean=false
+    println("$f :Boolean value")
+    var g:Long=893759498
+    println("$g :Long value")
+    var h:Short=9
+    println("$h :Short value")
+    var i:Byte=127
+    println("$i :Byte value")
+
+}
