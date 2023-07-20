8950e65f1602586dfa67acafe82a7fbeeb3c0159
JENNY <nandaniyajeni@gmail.com>
2023-07-20 23:22:34 +0530

    1.7

---
+++ practical1.7.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    println("21012011155-PARTH")
+    print("Enter Number:")
+    var number = readLine()!!.toInt()
+    val factorial = fact (number)
+    println("Factorial of $number = $factorial")
+
+}
+tailrec fun fact(n: Int,temp: Int = 1):Int{
+    return if (n == 1){
+        temp
+    }else{
+        fact (n-1,temp*n)
+    }
+}
