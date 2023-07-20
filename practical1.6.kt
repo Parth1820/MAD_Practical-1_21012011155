872869c0b2eff95f1fcb11f429b6d0a3209062bc
JENNY <nandaniyajeni@gmail.com>
2023-07-20 23:17:28 +0530

    1.6

---
+++ practical1.6.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("21012011155-PARTH")
+    var a= 12
+    var b= 22
+    var c= 32
+    var ans = a+b+c
+    var ans1 = a-b-c
+    var ans2 = a*b*c
+    var ans3 = b/a
+    println("Addition of $a $b $c $ans")
+    println("Subtraction of $a $b $c $ans1")
+    println("multiplication of $a $b $c $ans2")
+    println("Division of $b $a $ans3")
+
+}
