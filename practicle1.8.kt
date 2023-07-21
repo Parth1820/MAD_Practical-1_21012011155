3f7cfd46d2a59abe439948db73f55e1dda27624f
student <student@gnu.ac.in>
2023-07-21 08:55:22 +0530

    practicle1.8.kt

---
+++ practicle1.8.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+//fun main(){
+//    val num = arrayOf(1, 2, 3, 4)
+//    print(Arrays.toString(num))
+//}
+
+fun main(){
+    var b = Array<Int>(5){1}
+    print(Arrays.toString(b))
+}
+
